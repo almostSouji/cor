6063e4fb623a0e60c8abf0eff8b457a4e0cfa721
almostSouji <timoqueezle@gmail.com>
2019-06-01 01:02:56 +0200

    feat: extend TextChannel structure

---
+++ src/bot/extensions/textChannel.ts
@@ -0,0 +1,36 @@
+import {
+  Structures,
+  Guild,
+  Message,
+  MessageAttachment,
+  MessageEmbed
+} from "discord.js";
+
+declare module "discord.js" {
+  export interface TextChannel {
+    relayMessage(message: Message): Promise<Message | Message[]>;
+  }
+}
+
+export default Structures.extend(
+  "TextChannel",
+  (TextChannel): typeof TextChannel => {
+    class CorTextChannel extends TextChannel {
+      public constructor(guild: Guild, data: object) {
+        super(guild, data);
+      }
+
+      public relayMessage(message: Message): Promise<Message | Message[]> {
+        const additions: (MessageEmbed | MessageAttachment)[] = [];
+        for (const embed of message.embeds) {
+          additions.push(embed);
+        }
+        for (const attachment of message.attachments.values()) {
+          additions.push(attachment);
+        }
+        return this.send(message.content, additions);
+      }
+    }
+    return CorTextChannel;
+  }
+);
