42118165af24ff463d7bd1a54038045b2ac815b7
almostSouji <timoqueezle@gmail.com>
2019-07-13 12:08:54 +0200

    feat: help for specific command

--- src/bot/commands/util/help.ts
+++ src/bot/commands/util/help.ts
@@ -1,6 +1,7 @@
 import { Command, Category } from 'discord-akairo';
 import { Message, PermissionResolvable } from 'discord.js';
 import { stripIndents } from 'common-tags';
+import { CorEmbed } from '../../structures/CorEmbed';
 
 export default class HelpCommand extends Command {
 	private constructor() {
@@ -27,6 +28,49 @@ export default class HelpCommand extends Command {
 				}
 			]
 		});
+	}
+
+	private buildInfoEmbed(ref: Command, message: Message): CorEmbed {
+		let idString = `Name: \`${ref.id}\``;
+		if (ref.category) {
+			idString += `\nCategory: \`${ref.category}\``;
+		}
+		if (ref.aliases.length) {
+			idString += `\nAliases: ${ref.aliases.map(e => `\`${e}\``).join(', ')}`;
+		}
+		let infoString = '';
+		if (ref.description.content) {
+			infoString += `\nDescription: ${ref.description.content}`;
+		}
+		if (ref.description.usage) {
+			infoString += `\nUsage: \`${ref.description.usage}\``;
+		}
+		let restrictionString = '';
+		if (ref.ownerOnly) {
+			const check = this.client.isOwner(message.author);
+			restrictionString += `\n${check ? '`✅`' : '`❌`'} Owner only`;
+		}
+		if (ref.channel === 'guild') {
+			const check = message.channel.type === 'text';
+			restrictionString += `\n${check ? '`✅`' : '`❌`'} Command can only be used in a guild`;
+		}
+		if (ref.userPermissions) {
+			const perms = ref.userPermissions as PermissionResolvable[];
+			const check = message.channel.type === 'text' && message.member.permissions.has(perms);
+			restrictionString += `\n${check ? '`✅`' : '`❌`'} User permissions: ${perms.map(e => `\`${e}\``).join(', ')}`;
+		}
+
+
+		const embed = new CorEmbed()
+			.addField('Command Information', idString)
+			.addField('About', infoString);
+		if (restrictionString) {
+			embed.addField('Restrictions', restrictionString);
+		}
+		if (!embed.color && message.guild && message.guild.me.displayColor) {
+			embed.setColor(message.guild.me.displayColor);
+		}
+		return embed.applySpacers();
 	}
 
 	public async exec(message: Message, { cmd, all }: { cmd: Command; all: boolean }): Promise<any> {
@@ -84,5 +128,6 @@ export default class HelpCommand extends Command {
 			You can use \`${prefix}${this.id} <commandname>\` to get more information about a command.`;
 			return message.util!.send(commandString);
 		}
+		message.util!.send('', this.buildInfoEmbed(cmd, message));
 	}
 }
