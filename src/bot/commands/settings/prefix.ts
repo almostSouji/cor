dd9a2be44a5b5d2d6382617175bd30e875f00fa7
almostSouji <timoqueezle@gmail.com>
2019-07-25 09:10:33 +0200

    feat: prefix cmd

---
+++ src/bot/commands/settings/prefix.ts
@@ -0,0 +1,54 @@
+import { Command, Argument } from 'discord-akairo';
+import { Message, User } from 'discord.js';
+
+class PrefixCommand extends Command {
+	private constructor() {
+		super('prefix', {
+			aliases: [
+				'prefix',
+				'pr',
+				'pre'
+			],
+			description: {
+				content: 'Change prefix, `--reset` to reset to default',
+				usage: '[new prefix]'
+			},
+			ownerOnly: true,
+			args: [
+				{
+					id: 'prefix',
+					type: 'string'
+				},
+				{
+					id: 'sudo',
+					match: 'flag',
+					flag: ['--force', '-f', '--sudo']
+				},
+				{
+					id: 'reset',
+					match: 'flag',
+					flag: ['--reset', '-r']
+				}
+			]
+		});
+	}
+
+	public async exec(message: Message,	{ prefix, force, reset }: { prefix: string; force: boolean; reset: boolean }): Promise<Message | Message[]> {
+		const defaultPrefix = this.client.config.prefix;
+		if (message.channel.type !== 'text') {
+			return message.util!.send(`You can use the standard prefix \`${defaultPrefix}\` in direct messages.`);
+		}
+		const override = this.client.isOwner(message.author!) && force;
+		const oldPrefix = this.client.settings.get(message.guild!.id, 'prefix', defaultPrefix);
+		if ((!prefix && !reset) || (!message.member!.hasPermission('MANAGE_GUILD') && !override)) {
+			return message.util!.send(`My prefix here is \`${oldPrefix}\`.\nAlternatively you can mention me.`);
+		}
+		if (reset) {
+			await this.client.settings.set(message.guild!.id, 'prefix', defaultPrefix);
+			return message.util!.send(`Prefix on \`${message.guild!.name}\` reset to \`${defaultPrefix}\`.`);
+		}
+		await this.client.settings.set(message.guild!.id, 'prefix', prefix);
+		return message.util!.send(`Prefix on \`${message.guild!.name}\` changed from \`${oldPrefix}\` to \`${prefix}\`.`);
+	}
+}
+export default PrefixCommand;
