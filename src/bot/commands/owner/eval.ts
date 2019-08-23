e0131cd7491fa3d4fb65f2c596c14dfe46070806
almostSouji <timoqueezle@gmail.com>
2019-08-23 11:34:42 +0200

    improvement: eval handling

--- src/bot/commands/owner/eval.ts
+++ src/bot/commands/owner/eval.ts
@@ -1,16 +1,19 @@
 import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
-import * as UTIL from 'util';
-import { CorEmbed as COREMBED } from '../../structures/CorEmbed';
-import * as DATEFNS from 'date-fns';
-import * as FETCH from 'node-fetch';
-import * as CORUTIL from '../../util/';
+import * as Util from 'util';
+import { CorEmbed as Embed } from '../../structures/CorEmbed';
+import * as DateFns from 'date-fns';
+import * as fetch from 'node-fetch';
+import * as CorUtil from '../../util/';
 
 const cbStartJS = '```js\n';
 const cbStartXl = '```xl\n';
 const cbEnd = '\n```';
 
-class PingCommand extends Command {
+class EvalCommand extends Command {
+	public hrStart: [number, number] | undefined;
+	public lastResult: any = null;
+
 	private constructor() {
 		super('eval', {
 			aliases: ['eval', 'ev'],
@@ -31,7 +34,7 @@ class PingCommand extends Command {
 					}
 				},
 				{
-					id: 'input',
+					id: 'showInput',
 					match: 'flag',
 					flag: ['--input', '--in', '-i']
 				},
@@ -60,66 +63,83 @@ class PingCommand extends Command {
 				{
 					id: 'as',
 					match: 'flag',
-					flag: ['--async', '-a']
+					flag: ['--async', '--as', '-a']
 				}
 			]
 		});
 	}
 
-	public async exec(message: Message, { code, input, noout, haste, depth, del, as }: {code: string; input: boolean; noout: boolean; haste: boolean; depth: number; del: boolean; as: boolean }): Promise<Message | Message[] | void> {
-		function clean(text: string, token: string): string {
-			return text
-				.replace(/`/g, `\`${String.fromCharCode(8203)}`)
-				.replace(/@/g, `@${String.fromCharCode(8203)}`)
-				.replace(new RegExp(token, 'gi'), '*****');
+	private _clean(text: string, token: string): string {
+		return text
+			.replace(/`/g, `\`${String.fromCharCode(8203)}`)
+			.replace(/@/g, `@${String.fromCharCode(8203)}`)
+			.replace(new RegExp(token, 'gi'), '*****');
+	}
+
+	private async _result(result: any, hrDiff: [number, number], input: string | null = null, showinput: boolean | null = null, haste: boolean | null = null, depth: number = 0): Promise<string> {
+		const cleaned = this._clean(Util.inspect(result, { depth }), this.client.token);
+		let response = '';
+		if (showinput) {
+			response += `\nInput:${cbStartJS}${input}${cbEnd}`;
 		}
+
+		response += `Output:${cbStartJS}${cleaned}${cbEnd}`;
+		response += `• Type: \`${typeof result}\``;
+		response += ` • time taken: \`${(((hrDiff[0] * 1e9) + hrDiff[1])) / 1e6}ms\``;
+
+		if (haste) {
+			const hasteLink = await CorUtil.postHaste(cleaned, 'js');
+			response += `\n• Full Inspect: ${hasteLink}`;
+		}
+		return response;
+	}
+
+	public async exec(message: Message, { code, showInput, noout, haste, depth, del, as }: {code: string; showInput: boolean; noout: boolean; haste: boolean; depth: number; del: boolean; as: boolean }): Promise<Message | Message[] | void> {
 		/* eslint-disable @typescript-eslint/no-unused-vars */
-		const Embed = COREMBED;
-		const FateFns = DATEFNS;
-		const fetch = FETCH;
-		const corUtil = CORUTIL;
-		const util = UTIL;
+		const util = {
+			Util,
+			Embed,
+			DateFns,
+			fetch,
+			CorUtil
+		};
+		const msg = message;
+		const doReply = (val: any): any => {
+			if (val instanceof Error) {
+				return message.util!.send(`Callback error: \`${val}\``);
+			}
+			const result = this._result(val, process.hrtime(this.hrStart));
+			return message.util!.send(result);
+		};
 		/* eslint-enable @typescript-eslint/no-unused-vars */
-		let evaled;
+
 		try {
-			const hrStart = process.hrtime();
+			this.hrStart = process.hrtime();
 			if (as) {
 				const asyncCode = `(async () => { ${code} })()`;
-				evaled = await eval(asyncCode); // eslint-disable-line no-eval
+				this.lastResult = await eval(asyncCode); // eslint-disable-line no-eval
 			} else {
-				evaled = eval(code); // eslint-disable-line no-eval
+				this.lastResult = eval(code); // eslint-disable-line no-eval
 			}
-			if (evaled instanceof Promise) {
-				evaled = await evaled;
-			}
-			const hrStop = process.hrtime(hrStart);
-			let response = '';
-			if (input) {
-				response += `\nInput:${cbStartJS}${code}${cbEnd}`;
-			}
-
-			response += `Output:${cbStartJS}${clean(util.inspect(evaled, { depth }), this.client.token!)}${cbEnd}`;
-			response += `• Type: \`${typeof evaled}\``;
-			response += ` • time taken: \`${(((hrStop[0] * 1e9) + hrStop[1])) / 1e6}ms\``;
-
-			if (haste) {
-				const hasteLink = await corUtil.postHaste(clean(util.inspect(evaled), this.client.token!), 'js');
-				response += `\n• Full Inspect: ${hasteLink}`;
+			if (this.lastResult instanceof Promise) {
+				this.lastResult = await this.lastResult;
 			}
 			if (del && message.deletable) {
 				message.delete();
 			}
+			const hrStop = process.hrtime(this.hrStart);
+			const result = await this._result(this.lastResult, hrStop, code, showInput, haste, depth);
 			if (!noout) {
-				return await message.util!.send(response);
+				return await message.util!.send(result);
 			}
 		} catch (error) {
 			if (error.message.includes('Must be 2000 or fewer in length')) {
-				const hasteLink = await corUtil.postHaste(clean(util.inspect(evaled, { depth }), this.client.token!));
+				const hasteLink = await CorUtil.postHaste(this._clean(Util.inspect(this.lastResult, { depth }), this.client.token!));
 				return message.util!.send(`Output too long, trying to upload it to hastebin instead: ${hasteLink}`);
 			}
 			this.client.logger.info(`Eval error: ${error.stack}`);
-			return message.util!.send(`Error:${cbStartXl}${clean(error.stack, this.client.token!)}${cbEnd}`);
+			return message.util!.send(`Error:${cbStartXl}${this._clean(error.stack, this.client.token!)}${cbEnd}`);
 		}
 	}
 }
-export default PingCommand;
+export default EvalCommand;
