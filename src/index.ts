cb668339704e795c49e0da126150e42ab8138a5b
almostSouji <timoqueezle@gmail.com>
2019-10-13 00:58:27 +0200

    fix: reflect-metadata needs to be first import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import 'reflect-metadata';
 import { CorClient } from './bot/client/CorClient';
 import * as config from './bot/config.json';
-import 'reflect-metadata';
 
 const client: CorClient = new CorClient(config);
 client.start();
